fe41d021073039182811bcdf5fa1220a25522bb2
jamiejamiebobamie <jmccrory@vt.edu>
2020-04-29 20:55:14 -0700

    Initial Commit

---
+++ cpp_play/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_play
+//
+//  Created by James McCrory on 4/29/20.
+//  Copyright © 2020 James McCrory. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
